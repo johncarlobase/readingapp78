2908d828fb25b5de6e1a04743df211304c5674b5
John Carlo <carlobase@gmail.com>
2020-05-03 12:32:13 -0500

    mlab

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://<dbuser>:<dbpassword>@ds151048.mlab.com:51048/heroku_lkj8ng5w");
 
 // Start the API server
 app.listen(PORT, function() {
